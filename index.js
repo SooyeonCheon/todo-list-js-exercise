356fa64ceef1ff50598930ef4217f0bd19ca0d9e
SooyeonCheon123 <suyeon5445@gmail.com>
2023-02-17 11:41:16 -0800

    todo list OO

--- index.js
+++ index.js
@@ -1,3 +1,47 @@
+
+// Create a new task by adding to the arrays
+// A new task will be created as incomplete
+function newTask(title, description) {
+  const task = {
+    title: title,
+    description: description,
+    complete: false,
+
+    logState: function() {
+      console.log(`${this.title} has${this.complete ? " " : " not "}been completed`);
+    },
+
+    markCompleted: function() {
+      this.complete = true;
+    }
+  };
+  return task;
+}
+
+// prints out the provided task's details
+function logTaskState(task) {
+  console.log(`${task.title} has${task.complete ? " " : " not "}been completed`);
+}
+
+// marks the provided task as completed
+function completeTask(task) {
+  task.complete = true;
+}
+
+// DRIVER CODE BELOW
+
+const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
+const task2 = newTask("Do Laundry", "😨");
+const tasks = [task1, task2];
+
+task1.logState();
+task1.markCompleted();
+task1.logState();
+
+console.log(tasks);
+
+
+/*
 // Arrays to keep track of each task's state
 const taskTitles = [];
 const taskComplete = [];
@@ -29,3 +73,4 @@ newTask("Do Laundry"); // task 1
 logTaskState(0); // Clean Cat Litter has not been completed
 completeTask(0);
 logTaskState(0); // Clean Cat Litter has been completed
+*/
